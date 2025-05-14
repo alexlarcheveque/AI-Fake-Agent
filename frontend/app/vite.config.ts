577bbf56152eefd7fa5f9f62898c113960e54e79
Alex Larcheveque <alexlarcheveque@Mac.lan>
2025-05-13 20:50:24 -0700

    fix deployment failures

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "localhost:3000",
+        target: "https://localhost:3000",
         changeOrigin: true,
       },
     },
